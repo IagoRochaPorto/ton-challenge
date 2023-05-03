0393064703b9dd5ca644b8e13b3ca78673f4eb3e
Iago Rocha Porto <iagorochaporto@gmail.com>
2023-05-03 12:46:56 -0300

    feat: update e2e tests

--- __tests__/e2e/usersApi.test.ts
+++ __tests__/e2e/usersApi.test.ts
@@ -1,11 +1,13 @@
 import request from 'supertest'
+import crypto from 'node:crypto'
 
 describe('Users API', () => {
   const server = request('http://localhost:3000')
+  const username = crypto.randomBytes(20).toString('hex')
 
   it('Should return 201 on POST /user', async () => {
     const response = await server.post('/dev/user').send({
-      username: 'john doe',
+      username,
       password: 'zcDp%WDxs#j5e@B!X%hh@8',
       email: 'johndoe@email.com',
     })
@@ -13,21 +15,21 @@ describe('Users API', () => {
     expect(response.status).toBe(201)
     expect(response.body).toEqual({
       id: expect.any(String),
-      username: 'john doe',
+      username,
       email: 'johndoe@email.com',
     })
   })
 
   it('Should return 200 on POST /auth', async () => {
     const response = await server.post('/dev/auth?password=test123').send({
-      username: 'john doe',
+      username,
       password: 'zcDp%WDxs#j5e@B!X%hh@8',
     })
 
     expect(response.status).toBe(200)
     expect(response.body).toEqual({
       id: expect.any(String),
-      username: 'john doe',
+      username,
       email: 'johndoe@email.com',
     })
   })
@@ -36,5 +38,14 @@ describe('Users API', () => {
     const response = await server.get('/dev/user')
 
     expect(response.status).toBe(200)
+  })
+
+  it('Should return 400 on POST /user if user already exists', async () => {
+    const response = await server.post('/dev/user').send({
+      username,
+      password: 'zcDp%WDxs#j5e@B!X%hh@8',
+      email: 'johndoe@email.com',
+    })
+    expect(response.status).toBe(400)
   })
 })
